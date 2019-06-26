a9f9ce033c23dbf75ba8268c536d2f8aa76e18ae
Daniel Páez <danielpaezsw66@gmail.com>
2019-06-26 15:46:30 -0400

    Fetching users from public API

--- src/App.js
+++ src/App.js
@@ -1,49 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "./App.css"
 
-function App() {
+class App extends Component {
 
-  const pushRows = () => {
+  state = {
 
-    return [
-
-      <tr>
-        <td>Daniel</td>
-        <td>Heysoypaez@gmail.com</td>
-        <td>
-        <a href="http://heysoypaez.com">heysoypaez.com</a>
-        </td>
-      </tr>,
-      <tr>
-        <td>Paty</td>
-        <td>paty@gmail.com</td>
-        <td>
-        <a href="http://patytime.com">patytime.com</a>
-        </td>
-      </tr>
-    ]
+    users: [],
+    loading: false,
+    error: null
   }
 
-  return (
-    <div className="App">
-      <h1>Heysoypaez here</h1>
+  fetchUsers = async () => {
 
-      <table>
-        <thead>titulo
-         <tr>
-          <th>Nombre</th>
-          <th>Correo</th>
-          <th>Enlace</th>
-        </tr>
-        </thead>
+    try {
 
-        <tbody>
-          {pushRows()}
-        </tbody>
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    const response = await users.json()
 
-      </table>
-    </div>
-  );
+    this.setState({users: response })
+    
+    console.log(this.state)      
+    } 
+    catch(error) {
+
+    }
+
+  }
+
+  pushRows = () => (
+
+    this.state.users.map( (user) => (
+
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>
+        <a href={`http://${user.link || user.website}`}>{user.link || user.website}</a>
+        </td>
+      </tr>
+    ))
+  )
+
+  componentDidMount = () => {
+
+    this.fetchUsers()
+  }
+
+
+
+  render() {
+      return (
+        <div className="App">
+          <h1>Heysoypaez here</h1>
+  
+          <table>
+            <thead>
+             <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+            </thead>
+  
+            <tbody>
+              {this.pushRows()}
+            </tbody>
+  
+          </table>
+        </div>
+      );
+    }
 }
 
 export default App;
